9124b6b6f5255dfa20ce9ac707a9a13b4f591b08
Erin <erin2722@gmail.com>
2020-07-15 14:01:56 -0400

    :bug: fixes

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,21 +1,15 @@
 // Libraries
 import React, { useState, useEffect } from "react";
 import { CSSTransitionGroup } from 'react-transition-group';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 import '../App.css';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 
 
 const List = () => {
-    const [itemsList, setListData] = useState(
-        [
-            {word: 'Javascript', number: 92}, 
-            {word: 'API', number: 65}, 
-            {word: 'Pair-Programming', number: 46}, 
-            {word: 'Overcame', number: 34},
-            {word: 'Blocked', number: 20}
-        ]
-    );
+    const [itemsList, setListData] = useState('');
 
     useEffect(() => {
         fetch('/getWords').then(res => res.json()).then(data => {
@@ -23,14 +17,25 @@ const List = () => {
           console.log(data.words);
         });
       }, []);
-    
-    const items = itemsList.map((item, i) => (
-        <div key = {i} class="container">
-            <div class="level notification my-3 width-set hvr-grow">
-                <div class="mx-4">You referenced <strong>{item[0]}</strong> {item[1]} times.</div>
+
+    let items;
+
+    if(itemsList[0] !== undefined) {
+        items = itemsList.map((item, i) => (
+            <div key = {i} class="container">
+                <div class="level notification my-3 width-set hvr-grow">
+                    <div class="mx-4">You referenced <strong>{item[0]}</strong> {item[1]} times.</div>
+                </div>
             </div>
-        </div>
-    ));
+        ));
+    } else {
+        items = (
+            <div class="level notification my-3 width-set hvr-grow">
+                    <div class="mx-4">Fetching your results...</div>
+                    <FontAwesomeIcon icon={faSpinner} className="loading" />
+            </div>
+        );
+    }
 
     return (
         <div>
